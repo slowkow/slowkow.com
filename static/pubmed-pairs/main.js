381def1c70d8bd6fd7d7f3005675a05886e55b18
Kamil Slowikowski <kslowikowski@gmail.com>
2023-09-18 10:03:11 -0400

    update code for pubmed-pairs

--- static/pubmed-pairs/main.js
+++ static/pubmed-pairs/main.js
@@ -2,10 +2,121 @@
 import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm"
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
 
-import { on, count_papers, get_papers } from "./papers.js"
+// import { on, count_papers, get_papers } from "./pubmed-pairs/papers.js"
 
-window.count_papers = count_papers
-window.get_papers = get_papers
+// window.count_papers = count_papers
+// window.get_papers = get_papers
+
+//// TODO revise with Plot’s render transform (0.6.10)
+//function on(mark, listeners = {}) {
+//  const render = mark.render;
+//  mark.render = function (facet, { x, y }, channels) {
+//    // 🌶 I'd like to be allowed to read the facet
+//    // …  mutable debug = fx.domain()??
+//    // 🌶 data[i] may or may not be the datum, depending on transforms
+//    // (at this stage we only have access to the materialized channels we requested)
+//    // but in simple cases it works
+//    const data = this.data;
+//    // 🌶 since a point or band scale doesn't have an inverse, create one from its domain and range
+//    if (x && x.invert === undefined)
+//      x.invert = d3.scaleQuantize(x.range(), x.domain());
+//    if (y && y.invert === undefined)
+//      y.invert = d3.scaleQuantize(y.range(), y.domain());
+//    const g = render.apply(this, arguments);
+//    const r = d3.select(g).selectChildren();
+//    for (const [type, callback] of Object.entries(listeners)) {
+//      r.on(type, function (event, i) {
+//        const p = d3.pointer(event, g);
+//        callback(event, {
+//          type,
+//          p,
+//          datum: data[i],
+//          i,
+//          facet,
+//          data,
+//          ...(x && { x: x.invert(p[0]) }),
+//          ...(y && { y: y.invert(p[1]) }),
+//          ...(x && channels.x2 && { x2: x.invert(channels.x2[i]) }),
+//          ...(y && channels.y2 && { y2: y.invert(channels.y2[i]) })
+//        });
+//      });
+//    }
+//    return g;
+//  };
+//  return mark;
+//} 
+
+var count_papers_cache = {}
+
+async function count_papers(first, second) {
+  const term = `${first} ${second}`
+  if (term in count_papers_cache) {
+    resp = count_papers_cache[term]
+    return +resp.esearchresult.count
+  }
+  // const first = 'tocilizumab'
+  // const second = 'HLA-DQA1'
+  var resp = await (async function() {
+    const queryParams = {
+      email: 'kslowikowski@gmail.com',
+      usehistory: 'y', db: 'pubmed', term: term, retmode: 'json'
+    }
+    const queryString = new URLSearchParams(queryParams).toString()
+    console.log('fetch eutils esearch')
+    return await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?${queryString}`)
+      .then((response) => {
+        return response.json()
+      })
+  })()
+  count_papers_cache[term] = resp
+  return +resp.esearchresult.count
+}
+
+async function get_papers(first, second) {
+  // const first = 'tocilizumab'
+  // const second = 'HLA-DQA1'
+  var text = await (async function() {
+    const term = `${first} ${second}`
+    const queryParams = {
+      email: 'kslowikowski@gmail.com',
+      usehistory: 'y', db: 'pubmed', term: term, retmode: 'json'
+    }
+    const queryString = new URLSearchParams(queryParams).toString()
+    return await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?${queryString}`)
+      .then((response) => {
+        return response.json()
+      })
+      .then(async (d) => {
+        const pubmed_ids = d.esearchresult.idlist
+        const queryString = new URLSearchParams({
+          WebEnv: d.esearchresult.webenv,
+          db: 'pubmed',
+          rettype: 'abstract',
+          retmode: 'text',
+          id: Array.prototype.join.call(pubmed_ids)
+        }).toString()
+        return await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?${queryString}`)
+          .then((response) => {
+            return response.text()
+        })
+      })
+  })()
+  return text.split(/\n\s*\n\s*\n/).map((p) => {
+    const items = p.split(/\n\s*\n/)
+    const doi = items.find(value => /PMID/.test(value))
+    const pmid = doi.match(/PMID: (\d+)/)[1]
+    return {
+      year: items[0].match(/\d{4}/)[0] + '-01-01',
+      journal: items[0],
+      title: items[1],
+      authors: items[2],
+      institutions: items[3],
+      abstract: items[4],
+      pmid: pmid
+    }
+  })
+}
+
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
@@ -35,7 +146,8 @@ const click_search = async function() {
       retval.push({
         'pair': `${x} ${y}`, 'first': x, 'second': y, 'count': n
       })
-			await timer(500)
+      build_table(retval)
+			await timer(1000)
     }
   }
   return retval
@@ -60,15 +172,13 @@ async function build_table(data) {
 	for (const d of data) {
 		if (first) {
 			const papers = await get_papers(d.first, d.second)
-			console.log(papers)
 			build_papers(papers, d.pair)
 			first = false
 		}
 		var el = document.getElementById(d.pair)
 		el.onclick = async function() {
-			console.log(d.pair)
+			// console.log(d.pair)
 			const papers = await get_papers(d.first, d.second)
-			console.log(papers)
 			build_papers(papers, d.pair)
 		}
 	}
